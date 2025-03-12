3741c2725bc0f05183a71b379abd8bd8be31c50e
전영환 <jeon-younghwan@jeon-yeonghwan-ui-MacBookPro.local>
2025-03-13 00:12:55 +0900

    git init

---
+++ src/main/java/com/example/newsfeed2/Newsfeed2Application.java
@@ -0,0 +1,13 @@
+package com.example.newsfeed2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newsfeed2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Newsfeed2Application.class, args);
+	}
+
+}
